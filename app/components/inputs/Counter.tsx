d6b0b30b3437c88c776ccfb6f8b32c3183f89be8
G3n3sis-x <parisileonardo15@gmail.com>
2023-08-22 21:04:44 +0200

    commit

--- app/components/inputs/Counter.tsx
+++ app/components/inputs/Counter.tsx
@@ -24,7 +24,7 @@ const Counter = ({ title, subtitle, value, onChange }: CounterProps) => {
     }, [onChange, value])
     
     return (
-        <div className="flex flex-row items-center justify-between">
+        <div className="flex flex-row items-center justify-between select-none">
         <div className="flex flex-col">
           <div className="font-medium">{title}</div>
           <div className="font-light text-gray-600">
